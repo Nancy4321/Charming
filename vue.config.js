bc15da2ca094a038733d898bb4b551bc9f9cc8d9
Alberta <58301332+Nancy4321@users.noreply.github.com>
2021-12-17 22:15:43 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Charming/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Charming/public/index.html" : "/",
 }
